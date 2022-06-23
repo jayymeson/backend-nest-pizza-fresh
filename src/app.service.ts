70822eb1bae45d3e0bccfc03554499f0d85010d1
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-06-23 15:21:38 -0300

    chore(app): `implemente endpoint for app status`

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAppStatus(): string {
+    return 'Server is running 🚀👩‍🚀\n Please check! https://localhost:3001/api for Swagger docs.';
   }
 }
