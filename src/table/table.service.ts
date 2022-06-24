07f6f4a1bac8cd01933908d97c312d33e8726777
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-06-23 21:04:33 -0300

    feature(table): `create DTO`

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTableDTO } from './dto/create-table.dto';
 
 @Injectable()
 export class TableService {
-  create() {
-    return 'Create one table';
+  create(createTableDto: CreateTableDTO) {
+    return 'Create one table' + JSON.stringify(createTableDto);
   }
   findAll() {
     return 'Find all tables';
