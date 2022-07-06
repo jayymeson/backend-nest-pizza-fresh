5d84e67f85fd9b6b08a5eadafd97f8130eda4f68
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-07-06 16:48:38 -0300

    feature(orders):

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,34 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('order')
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Create an order',
+  })
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.orderService.create(createOrderDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'List all products',
+  })
   findAll() {
     return this.orderService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'List a product',
+  })
   findOne(@Param('id') id: string) {
-    return this.orderService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.orderService.update(+id, updateOrderDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.orderService.remove(+id);
+    return this.orderService.findOne(id);
   }
 }
