4dab2fdf83df4ee77e0af8ef84353b82cd8d95fd
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-07-07 21:47:44 -0300

    feature(categories):

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,33 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { Category } from './entities/category.entity';
+import { handleErrorConstraintUnique } from 'src/utils/handle-error.util';
 
 @Injectable()
 export class CategoriesService {
+  userSelect: any;
   constructor(private readonly prisma: PrismaService) {}
 
-  create(createCategoryDto: CreateCategoryDto): Promise<Category> {
-    return this.prisma.category.create({ data: createCategoryDto });
+  async create(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    try {
+      return await this.prisma.category.create({ data: createCategoryDto });
+    } catch (error) {
+      return handleErrorConstraintUnique(error);
+    }
   }
 
   findAll(): Promise<Category[]> {
     return this.prisma.category.findMany();
   }
 
-  findOne(id: string): Promise<Category> {
-    return this.prisma.category.findUnique({ where: { id: id } });
-  }
-
-  update(id: string, updateCategoryDto: UpdateCategoryDto): Promise<Category> {
-    return this.prisma.category.update({
-      where: { id: id },
-      data: updateCategoryDto,
+  async verifyingTheCategory(id: string): Promise<Category> {
+    const category: Category = await this.prisma.category.findUnique({
+      where: { id },
     });
+
+    if (!category) {
+      throw new NotFoundException(`ID record '${id}' not found`);
+    }
+
+    return category;
   }
 
-  remove(id: string) {
+  findOne(id: string): Promise<Category> {
+    return this.verifyingTheCategory(id);
+  }
+
+  async update(
+    id: string,
+    updateCategoryDto: UpdateCategoryDto,
+  ): Promise<Category | void> {
+    await this.verifyingTheCategory(id);
+
+    return this.prisma.category
+      .update({
+        where: { id: id },
+        data: updateCategoryDto,
+      })
+      .catch(handleErrorConstraintUnique);
+  }
+
+  async remove(id: string) {
+    await this.verifyingTheCategory(id);
     return this.prisma.category.delete({
       where: { id: id },
       select: { name: true },
