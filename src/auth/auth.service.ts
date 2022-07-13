537fbe40ef825b02958d87770f6b69b1b26ffb66
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-07-13 20:54:24 -0300

    feature(auth):

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,16 @@
-import {
-  Injectable,
-  NotFoundException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginResponseDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
   async login(loginDto: LoginDto): Promise<LoginResponseDto> {
     const { nickname, password } = loginDto;
 
@@ -31,7 +31,7 @@ export class AuthService {
     delete user.password;
 
     return {
-      token: 'Test',
+      token: this.jwtService.sign({ nickname }),
       user,
     };
   }
